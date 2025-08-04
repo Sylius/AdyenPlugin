f067d94c7e4028f3cbdc08fef42b05c83ae9aa7f
Grzegorz Sadowski <sadowskigp@gmail.com>
2025-08-04 13:15:37 +0200

    Adjust DoctrineHandler to the newest version of Monolog

--- src/Logging/Monolog/DoctrineHandler.php
+++ src/Logging/Monolog/DoctrineHandler.php
@@ -14,28 +14,20 @@ declare(strict_types=1);
 namespace Sylius\AdyenPlugin\Logging\Monolog;
 
 use Monolog\Handler\AbstractProcessingHandler;
+use Monolog\LogRecord;
 use Sylius\AdyenPlugin\Factory\LogFactoryInterface;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
 
 final class DoctrineHandler extends AbstractProcessingHandler
 {
-    /** @var LogFactoryInterface */
-    private $logFactory;
-
-    /** @var RepositoryInterface */
-    private $repository;
-
     public function __construct(
-        LogFactoryInterface $logFactory,
-        RepositoryInterface $repository,
+        private readonly LogFactoryInterface $logFactory,
+        private readonly RepositoryInterface $repository,
     ) {
-        $this->logFactory = $logFactory;
-        $this->repository = $repository;
-
         parent::__construct();
     }
 
-    protected function write(array $record): void
+    protected function write(LogRecord $record): void
     {
         $log = $this->logFactory->create($record['message'], $record['level'], 0);
 
