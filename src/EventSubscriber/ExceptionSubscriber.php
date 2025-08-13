5e2de553c8081f9ab9f18d0d0a5d3e1515978f7b
Rafikooo <thecrazyrafik@gmail.com>
2025-08-14 00:28:26 +0200

    Add ExceptionSubscriber to handle Adyen and UnprocessablePayment exceptions

---
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Sylius\AdyenPlugin\EventSubscriber;
+
+
+use Adyen\AdyenException;
+use Sylius\AdyenPlugin\Exception\UnprocessablePaymentException;
+use Sylius\AdyenPlugin\Resolver\Notification\NotificationResolver\NoCommandResolvedException;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class ExceptionSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::EXCEPTION => ['onKernelException', 100],
+        ];
+    }
+
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+
+        if ($exception instanceof UnprocessablePaymentException) {
+            $event->setResponse(new JsonResponse([
+                'error' => 'Unprocessable Payment',
+                'details' => $exception->getMessage(),
+            ], Response::HTTP_UNPROCESSABLE_ENTITY));
+        }
+
+        if ($exception instanceof AdyenException) {
+            $event->setResponse(new JsonResponse([
+                'error' => $exception->getMessage(),
+                'details' => [
+                    'errorCode' => $exception->getAdyenErrorCode(),
+                    'status' => $exception->getStatus(),
+                    'errorType' => $exception->getErrorType(),
+                    'pspReference' => $exception->getPspReference(),
+                ]
+            ], Response::HTTP_UNPROCESSABLE_ENTITY));
+        }
+
+        if ($exception instanceof NoCommandResolvedException) {
+            $event->setResponse(new JsonResponse([
+                'error' => 'No command resolved for notification',
+                'details' => $exception->getMessage(),
+            ], Response::HTTP_UNPROCESSABLE_ENTITY));
+        }
+    }
+}
