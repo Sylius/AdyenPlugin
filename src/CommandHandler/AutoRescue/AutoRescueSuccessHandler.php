c5c2428b9ab4d2385857c67f4c74b39666485785
Rafikooo <thecrazyrafik@gmail.com>
2025-08-14 00:33:20 +0200

    Introduce AutoRescue commands and handlers for Adyen plugin

---
+++ src/CommandHandler/AutoRescue/AutoRescueSuccessHandler.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the Sylius Adyen Plugin package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\AdyenPlugin\CommandHandler\AutoRescue;
+
+use Sylius\AdyenPlugin\Command\AutoRescue\AutoRescueSuccess;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+
+#[AsMessageHandler]
+final class AutoRescueSuccessHandler
+{
+    public function __invoke(AutoRescueSuccess $command): void
+    {
+    }
+}
