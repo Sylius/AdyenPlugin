ad2273511049d02426d9d269424ba44637011d19
Rafikooo <thecrazyrafik@gmail.com>
2025-08-19 16:28:41 +0200

    [Refactor] Make Amount immutable with constructor and readonly properties

--- src/Resolver/Notification/Struct/Amount.php
+++ src/Resolver/Notification/Struct/Amount.php
@@ -13,11 +13,20 @@ declare(strict_types=1);
 
 namespace Sylius\AdyenPlugin\Resolver\Notification\Struct;
 
-class Amount
+/**
+ * Value object representing an amount in Adyen minor units.
+ *
+ * - $currency is an ISO 4217 code (e.g. "EUR").
+ * - $value is an integer in minor units (e.g. 1234 => 12.34 EUR).
+ */
+final class Amount
 {
-    /** @var ?string */
-    public $currency;
-
-    /** @var ?int */
-    public $value;
+    public function __construct(
+        public readonly string $currency,
+        public readonly int $value,
+    ) {
+        if (!\preg_match('/^[A-Z]{3}$/', $this->currency)) {
+            throw new \InvalidArgumentException('Currency must be a 3-letter ISO 4217 code.');
+        }
+    }
 }
