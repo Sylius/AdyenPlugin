ae49bea65161cb9874ce812d4c81a82186c580af
Jan Goralski <jan.wojciech.goralski@gmail.com>
2025-08-25 14:24:15 +0200

    [Maintenance] Abstract out notification data aware commands creation

---
+++ src/Bus/Command/NotificationDataAwarePaymentCommand.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the Sylius Adyen Plugin package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\AdyenPlugin\Bus\Command;
+
+/**
+ * Marker interface for commands taking payment and notification data in the constructor
+ *
+ * @see PaymentCommandFactory
+ */
+interface NotificationDataAwarePaymentCommand
+{
+}
