1f14ecfbcffee5ec2883afe91be57c82d181b582
Rafikooo <thecrazyrafik@gmail.com>
2025-09-04 01:20:01 +0200

    [Refactor] Use ShopperReferenceResolver in PaymentMethodsProvider

--- src/Provider/PaymentMethodsProvider.php
+++ src/Provider/PaymentMethodsProvider.php
@@ -13,20 +13,17 @@ declare(strict_types=1);
 
 namespace Sylius\AdyenPlugin\Provider;
 
-use Sylius\AdyenPlugin\Bus\Query\GetToken;
-use Sylius\AdyenPlugin\Entity\AdyenTokenInterface;
 use Sylius\AdyenPlugin\Exception\AdyenNotFoundException;
 use Sylius\AdyenPlugin\Filter\PaymentMethodsFilterInterface;
 use Sylius\AdyenPlugin\Filter\StoredPaymentMethodsFilterInterface;
 use Sylius\AdyenPlugin\Mapper\PaymentMethodsMapperInterface;
 use Sylius\AdyenPlugin\Model\PaymentMethodData;
 use Sylius\AdyenPlugin\Repository\PaymentMethodRepositoryInterface;
+use Sylius\AdyenPlugin\Resolver\ShopperReferenceResolverInterface;
 use Sylius\AdyenPlugin\Traits\GatewayConfigFromPaymentTrait;
 use Sylius\Component\Core\Model\CustomerInterface;
 use Sylius\Component\Core\Model\OrderInterface;
-use Sylius\Component\Core\Model\PaymentMethodInterface;
 use Symfony\Component\Messenger\HandleTrait;
-use Symfony\Component\Messenger\MessageBusInterface;
 
 final class PaymentMethodsProvider implements PaymentMethodsProviderInterface
 {
@@ -39,22 +36,28 @@ final class PaymentMethodsProvider implements PaymentMethodsProviderInterface
         private readonly PaymentMethodsFilterInterface $paymentMethodsFilter,
         private readonly StoredPaymentMethodsFilterInterface $storedPaymentMethodsFilter,
         private readonly PaymentMethodsMapperInterface $paymentMethodsMapper,
-        MessageBusInterface $messageBus,
+        private readonly ShopperReferenceResolverInterface $shopperReferenceResolver,
     ) {
-        $this->messageBus = $messageBus;
     }
 
     public function provideForOrder(string $paymentMethodCode, OrderInterface $order): PaymentMethodData
     {
         $paymentMethod = $this->paymentMethodRepository->getOneAdyenForCode($paymentMethodCode);
+
         if ($paymentMethod === null) {
             throw new AdyenNotFoundException(sprintf('Payment method "%s" not found.', $paymentMethodCode));
         }
 
-        $token = $this->getToken($paymentMethod, $order);
+        /** @var CustomerInterface $customer */
+        $customer = $order->getCustomer();
+
+        $shopperReference = $customer !== null && $customer->hasUser() === true
+            ? $this->shopperReferenceResolver->resolve($paymentMethod, $customer)
+            : null;
+
         $client = $this->adyenClientProvider->getClientForCode($paymentMethodCode);
 
-        $response = $client->getPaymentMethodsResponse($order, $token);
+        $response = $client->getPaymentMethodsResponse($order, $shopperReference);
 
         $available = $this->paymentMethodsMapper->mapAvailable($response->getPaymentMethods());
         $available = $this->paymentMethodsFilter->filter($available);
@@ -64,19 +67,5 @@ final class PaymentMethodsProvider implements PaymentMethodsProviderInterface
             paymentMethods: $available,
             storedPaymentMethods: $this->storedPaymentMethodsFilter->filterAgainstAvailable($stored, $available),
         );
-    }
-
-    private function getToken(PaymentMethodInterface $paymentMethod, OrderInterface $order): ?AdyenTokenInterface
-    {
-        /** @var ?CustomerInterface $customer */
-        $customer = $order->getCustomer();
-        if ($customer === null || !$customer->hasUser()) {
-            return null;
-        }
-
-        /** @var AdyenTokenInterface $token */
-        $token = $this->handle(new GetToken($paymentMethod, $order));
-
-        return $token;
     }
 }
