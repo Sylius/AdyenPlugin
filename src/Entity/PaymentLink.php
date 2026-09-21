d4f1389b716226efd1f6f668dcc2cdaa54beaebb
TheMilek <kam.grygierzec@gmail.com>
2026-09-21 11:29:32 +0200

    Fix CI failures on Symfony 6.4, Sylius 2.3 and immutable payment link date

--- src/Entity/PaymentLink.php
+++ src/Entity/PaymentLink.php
@@ -19,7 +19,7 @@ class PaymentLink implements PaymentLinkInterface
 {
     private ?int $id = null;
 
-    private readonly \DateTimeInterface $createdAt;
+    private readonly \DateTimeImmutable $createdAt;
 
     public function __construct(
         private readonly PaymentInterface $payment,
